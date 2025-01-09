a5a63c351fbb403445c62f581595ea6cf2f44c96
Joshua Gonzales <jgonzales.ibarrola@gmail.com>
2025-01-09 22:04:13 +0800

    feat: Learned Middleware & Static Files

--- app.js
+++ app.js
@@ -4,6 +4,19 @@ const app = express();
 
 app.listen(3000);
 
+// middleware & static files
+app.use((req, res, next) => {
+  console.log('host: ', req.hostname);
+  console.log('path: ', req.path);
+  console.log('method: ', req.method);
+  next();
+})
+app.use((req, res, next) => {
+  console.log('in the next middleware');
+  next();
+})
+app.use((express.static('public')))
+
 app.get('/', (req, res) => {
   res.sendFile('./views/index.html', { root: __dirname });
 })
