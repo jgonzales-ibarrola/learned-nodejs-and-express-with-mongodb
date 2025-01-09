9bccd1b5c378c9a0aa43fb8a24479635d78bf899
Joshua Gonzales <jgonzales.ibarrola@gmail.com>
2025-01-09 13:30:21 +0800

    feat: Learned to Requests & Responses

--- server.js
+++ server.js
@@ -1,7 +1,40 @@
 const http = require('http')
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-  console.log('request made');
+  console.log(req.url, req.method);
+
+  res.setHeader('Content-Type', 'text/html');
+
+  let path = './views/';
+
+  switch (req.url) {
+    case '/':
+      path += 'index.html';
+      res.statusCode = 200;
+      break;
+    case '/blog':
+      path += 'blog.html';
+      res.statusCode = 200;
+      break;
+    case '/blog-x':
+      path += 'blog.html';
+      res.statusCode = 301;
+      res.setHeader('Location', '/blog');
+      break;
+    default:
+      path += '404.html'
+      res.statusCode = 404;
+  }
+  
+  fs.readFile(path, (err, data) => {
+    if (err) {
+      console.log(err);
+      res.end();
+    } else {
+      res.end(data);
+    }
+  })
 })
 
 server.listen(3000, 'localhost', () => {
