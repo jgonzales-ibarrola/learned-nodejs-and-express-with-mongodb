acb90ccbf9377aa629b502a986fab39c286d8496
Joshua Gonzales <jgonzales.ibarrola@gmail.com>
2025-01-09 12:01:34 +0800

    feat: Learned to Write in Files

--- files.js
+++ files.js
@@ -1,17 +1,26 @@
-const fs = require('fs');
+const fs = require("fs");
 
 // reading files
-fs.readFile('./docs/blog1.txt', (err, data) => {
-  if (err) {
-    console.log(err);
-  }
+// fs.readFile("./docs/blog1.txt", (err, data) => {
+// 	if (err) {
+// 		console.log(err);
+// 	}
 
-  console.log(data.toString());
-})
+// 	console.log(data.toString());
+// });
 
-console.log('last line');
+// console.log("last line");
 
-// output: 
+// output:
 // last line
 // Gojo Tosoru
 // because fs.readFile is not async, it just go to the nextline if reading takes time
+
+// writing files
+fs.writeFile("./docs/blog1.txt", "A new line of text!", () => {
+	console.log("file was written!");
+});
+
+fs.writeFile("./docs/blog2.txt", "A new file has been created!", () => {
+	console.log("A new file has been created!");
+});
