e55650554634cf06bf8cc0da6ec724b9f6f36788
Joshua Gonzales <jgonzales.ibarrola@gmail.com>
2025-01-09 22:36:13 +0800

    feat: Learned MongoDB Basics

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const Blog = require('./models/blog.js');
 
 const app = express();
+
+const dbURI = "mongodb+srv://jogo:jogo1234@cluster0.sd03q.mongodb.net/node-tuts?retryWrites=true&w=majority&appName=Cluster0"
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) => console.log('connected to DB'))
+  .catch((error) => console.log(error));
 
 app.listen(3000);
 
@@ -15,8 +22,40 @@ app.use((req, res, next) => {
   console.log('in the next middleware');
   next();
 })
+// Static Files
 app.use((express.static('public')))
 
+app.get('/add-blog', (req, res) => {
+  const blog = new Blog({
+    title: '2new Blog',
+    snippet: '2about Blog',
+    body: '2test body'
+  })
+
+  blog.save()
+    .then((result) => {
+      res.send(result)
+    })
+    .catch((err) => console.log(err));
+})
+
+app.get('/all-blogs', (req, res) => {
+  Blog.find()
+    .then((result) => {
+      res.send(result)
+    })
+    .catch((err) => console.log(err))
+})
+
+app.get('/single-blog', (req,res) => {
+  Blog.findById('677fdd63e495a81cbb6ee30d')
+  .then((result) => {
+    res.send(result)
+  })
+  .catch((err) => console.log(err))
+})
+
+// Routes
 app.get('/', (req, res) => {
   res.sendFile('./views/index.html', { root: __dirname });
 })
